d2a2e8d403a2de603125525df5f4da4b512a47ab
최민지 <choimj0728@douzone.com>
2019-08-22 17:15:04 +0900

    관리자 UI 추가

--- src/Pages/Management/Management.js
+++ src/Pages/Management/Management.js
@@ -8,21 +8,40 @@ import Menu from "./Menu";
 import styled from "styled-components";
 
 const Root = styled.div`
-  border: 1px solid red;
+  /* border: 1px solid red; */
   width: 90%;
   margin: auto;
-  padding: 20px;
+  /* padding: 20px; */
   height: 93vh;
+  position: relative;
+`;
+const LeftArea = styled.div`
+  width: 20%;
+  border: 1px solid black;
+  position: absolute;
+  top: 20px;
+  left: 0;
+`;
+const RightArea = styled.div`
+  width: 75%;
+  position: absolute;
+  top: 20px;
+  right: 0;
+  
 `;
 
 const Management = ({match}) => {
   // console.log(match);
   return (
     <Root>
-      <Menu match={match}/>      
-      <Route exact path={`${match.url}/category`} component={CategoryManagement}/>
-      <Route exact path={`${match.url}/room`} component={RoomManagement}/>
-      <Route exact path={`${match.url}/bookform`} component={BookFormManagement}/>
+      <LeftArea>
+        <Menu match={match}/>      
+      </LeftArea>
+      <RightArea>
+        <Route exact path={`${match.url}/category`} component={CategoryManagement}/>
+        <Route exact path={`${match.url}/room`} component={RoomManagement}/>
+        <Route exact path={`${match.url}/bookform`} component={BookFormManagement}/>
+      </RightArea>
     </Root>
   );
 }
